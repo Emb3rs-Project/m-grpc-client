85843539bf3daf12fd382865982feae9ba61855a
David Fernandes <david.fernandes@pdmfc.com>
2022-04-26 22:43:20 +0300

    updates

---
+++ data/cf_greenhouse.py
@@ -0,0 +1,19 @@
+cf_data = {
+    "location": [41.1045, -8.3539],
+    "greenhouse_orientation": "S",
+    "width": 23,
+    "length": 50,
+    "height": 2.64,
+    "shutdown_periods": [],
+    "daily_periods": [
+        [
+            0,
+            24
+        ]
+    ],
+    "lights_on": 0,
+    "T_heat_on": 18,
+    "T_cool_on": 35,
+    "thermal_blanket": 0,
+    "greenhouse_efficiency": 1
+}
--- entry.py
+++ entry.py
@@ -7,7 +7,7 @@ from cf.cf_models import ConvertSinkOutputModel
 
 import dotenv
 
-from data.cf_convert_source import cf_data
+from data.cf_convert_orc import cf_data
 
 
 dotenv.load_dotenv()
@@ -18,11 +18,7 @@ def use_case():
         os.getenv("MARKET_HOST"), os.getenv("MARKET_PORT"))
     cf_client = CFModuleClient(os.getenv("CF_HOST"), os.getenv("CF_PORT"))
 
-    result = cf_client.convert_source(
-        platform=cf_data["platform"],
-        cf_module=cf_data["cf-module"],
-        gis_module={}
-    )
+    result = cf_client.convert_orc(cf_data)
 
     print(f"{result}")
 
